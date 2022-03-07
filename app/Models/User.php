7d21758f076733dd6c879dfbdbf1ad696cd38afb
lswart <lauren.swart@gmail.com>
2022-03-07 13:27:18 +0100

    installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
+        'role_ref',
         'password',
     ];
 
@@ -41,4 +43,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table = 'users';
 }
