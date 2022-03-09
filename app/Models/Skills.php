22a8f12eee8338628b59153ff2313f571e615993
lswart <lauren.swart@gmail.com>
2022-03-09 14:06:30 +0100

    Skills: model, migration

---
+++ app/Models/Skills.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+    protected $table = 'skills';
+
+    protected $primaryKey = 'skill_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'course_ref',
+        'title',
+        'description'
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2022_03_09_130103_create_skills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->id('skill_id');
+            $table->foreignId('course_ref');
+            $table->string('title', 100);
+            $table->string('description', 255);
+
+            $table->foreign('course_ref')->references('course_id')->on('courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('skills');
+    }
+};
