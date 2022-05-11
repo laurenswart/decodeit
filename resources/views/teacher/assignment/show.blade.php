f62e928ffb990db2aaf9547faf0e426d275fc735
lswart <lauren.swart@gmail.com>
2022-05-11 16:51:00 +0200

    ui student teacherShow

--- resources/views/teacher/assignment/show.blade.php
+++ resources/views/teacher/assignment/show.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.teacher')
 
 @section('content')
-<section>
+	<nav class="back-nav">
+		<a href="{{ route('chapter_teacherShow', $assignment->chapters[0]->id) }}"><i class="fas fa-arrow-alt-square-left"></i>{{ $assignment->chapters[0]->title}}</a>
+	</nav>
 
 		<h2 class="light-card block-title layer-2">{{ $assignment->title }}</h2>
 		<div class="row">
 			<div class="form-section layer-2 col mx-2">
 				<h3 class="title-3">Associated Skills</h3>
 				@if(count($assignment->skills) >0)
-					@foreach($assignment->skills as $skill)
-					<ul>
-						<li>{{ $skill->title }}</li>
-					</ul>
-					@endforeach
+					<table class="table">
+						<tbody>
+						@foreach($assignment->skills->sort() as $skill)
+							<tr>
+								<td>{{ $skill->title }}</td>
+							</tr>
+						@endforeach
+						</tbody>
+					</table>
 				@else
 					<p>No skills</p>
 				@endif
@@ -22,23 +28,23 @@
 				<h3 class="title-3">Manage</h3>
 				<div class="label-value">
 					<span>Course</span>
-					<span>{{ $assignment->course->title }}</span>
+					<span><a href="{{ route('course_teacherShow', $assignment->course_id) }}">{{ $assignment->course->title }}</a></span>
 				</div>
 				<div class="label-value">
 					<span>Chapter</span>
-					<span>{{ $assignment->chapters[0]->title }}</span>
+					<span><a href="{{ route('chapter_teacherShow', $assignment->chapters[0]->id) }}">{{ $assignment->chapters[0]->title }}</a></span>
 				</div>
 				<div class="label-value">
 					<span>Created</span>
 					<span>{{ $assignment->created_at }}</span>
 				</div>
-				<div class="label-value">
+				<div class="label-value mb-3">
 					<span>Last Updated</span>
 					<span>{{ $assignment->updated_at }}</span>
 				</div>
-				<div class="label-value mt-4">
-					<span><a href="{{ route('assignment_teacherEdit', $assignment->id) }}"><i class="fas fa-pen-square"></i>Edit Assignment</a></span>
-					<span><a href="{{ route('assignment_teacherConfirmDelete', $assignment->id) }}"><i class="fas fa-trash-alt"></i>Delete Assignment</a></span>
+				<div class="d-flex flex-col align-items-end mt-3">
+					<a href="{{ route('assignment_teacherEdit', $assignment->id) }}"><i class="fas fa-pen-square"></i>Edit Assignment</a>
+					<a href="{{ route('assignment_teacherConfirmDelete', $assignment->id) }}"><i class="fas fa-trash-alt"></i>Delete Assignment</a>
 				</div>
 			</div>
 		</div>
@@ -63,11 +69,11 @@
 				</div>
 				<div class="label-value mt-3">
 					<span>Start</span>
-					<span>{{ $assignment->start_time }}</span>
+					<span>{{ date('D d/m/Y, H:i', strtotime($assignment->start_time)) }}</span>
 				</div>
 				<div class="label-value mt-3">
 					<span>End</span>
-					<span>{{ $assignment->end_time }}</span>
+					<span>{{ date('D d/m/Y, H:i', strtotime($assignment->end_time)) }}</span>
 				</div>
 				<div class="label-value mt-3">
 					<span>Is a Test</span>
@@ -101,9 +107,9 @@
 			@if(count($assignment->notes)==0)
 			<p>No notes have been added.</p>
 			@else
-				@foreach($assignment->notes as $note)
+				@foreach($assignment->notes->sortBy('created_at') as $note)
 					<div class="msg">
-						<span class="date">{{ $note->created_at}}</span>
+						<span class="date">{{ date('D d/m/Y, H:i', strtotime($note->created_at)) }}</span>
 						<p>{{ $note->content}}</p>
 					</div>
 				@endforeach
@@ -116,42 +122,40 @@
 		<h2 class="light-card block-title layer-2">Student Submissions</h2>
 		<div class="form-section layer-2   mx-2">
 		@if(count($assignment->studentAssignments)==0)
-			
 				<p>No submissions</p>
-			
 		@else	
 			
 			<table class="table">
               <thead>
                 <tr>
                   <th scope="col">Student</th>
-                  <th scope="col">Submissions</th>
-				  <th scope="col">Questions to Answer</th>
-                  <th scope="col">Mark</th>
+                  <th scope="col" class="cell-center">Submissions</th>
+				  <th scope="col" class="cell-center">Questions to Answer</th>
+                  <th scope="col" class="cell-center">Mark</th>
 				  <th></th>
                 </tr>
               </thead>
               <tbody>
 			  @foreach($assignment->studentAssignments as $studentAssignment)
                 <tr>
-                   <th scope="row">{{ ucfirst($studentAssignment->enrolment->student->firstname) }} {{ ucfirst($studentAssignment->enrolment->student->lastname) }}</th>
-                   <td>{{ count($studentAssignment->submissions) ?? '-'}}</td>
-				   <td>
+                   <th scope="row"><a href="{{ route('student_teacherShow', $studentAssignment->enrolment->student_id) }}">{{ ucfirst($studentAssignment->enrolment->student->firstname) }} {{ ucfirst($studentAssignment->enrolment->student->lastname) }}</th>
+                   <td class="cell-center">{{ count($studentAssignment->submissions) ?? '-'}}</td>
+				   <td class="cell-center">
 						@if(count($studentAssignment->submissions->whereNull('feedback')->whereNotNull('question')))
 							<i class="fas fa-exclamation-square"></i>
 						@else
 							-
 						@endif
 					</td>
-				   <td>
+				   <td class="cell-center">
 					@if($studentAssignment->mark) 
 						{{$studentAssignment->mark}} / {{ $assignment->max_mark}}
 					@elseif($studentAssignment->canBeMarked()) 
-						To do<i class="fas fa-exclamation-square"></i>
+						<i class="fas fa-exclamation-square"></i>To do
 					@else 
 						-
 					@endif</td>
-					<td><a href="{{ route('studentAssignment_teacherShow', $studentAssignment->id) }}"><i class="fas fa-arrow-alt-square-right greyed"></i>Manage</a></td>
+					<td><a href="{{ route('studentAssignment_teacherShow', $studentAssignment->id) }}"><i class="fas fa-arrow-alt-square-right"></i>Manage Submissions</a></td>
                 </tr>
 				@endforeach
               </tbody>
@@ -159,7 +163,6 @@
 			
 		@endif
 		</div>
-	</section>
 
 @endsection
 
--- resources/views/teacher/student/show.blade.php
+++ resources/views/teacher/student/show.blade.php
@@ -2,28 +2,12 @@
 
 
 @section('content')
-
+  <nav class="back-nav">
+			<a href="{{ route('student_teacherIndex') }}"><i class="fas fa-arrow-alt-square-left"></i>All Students</a>
+		</nav>
 		<h2 class=" block-title light-card layer-2">{{ ucfirst($student->firstname) }} {{ ucfirst($student->lastname) }}</h2>
 
     <div class="row">
-      <div class="col-12 col-xl-5 form-section layer-2 d-flex flex-col">
-            <div class="label-value">
-              <h4>Firstname</h4>
-              <span>{{ $student->firstname }}</span>
-            </div>
-            <div class="label-value">
-              <h4>Lastname</h4>
-              <span>{{ $student->lastname }}</span>
-            </div>
-            <div class="label-value">
-              <h4>Email</h4>
-              <span>{{ $student->email }}</span>
-            </div>
-            <div class="d-flex justify-content-between">
-              <a href="{{ route('student_teacherDownloadReport', $student->id) }}"><i class="fas fa-arrow-alt-to-bottom"></i>Download Report</a>
-              <a href="{{ route('student_teacherConfirmDelete', $student->id) }}"><i class="fas fa-trash-alt"></i>Remove Student</a>
-            </div>
-      </div>
       <div class="col form-section layer-2 ml-4 d-flex flex-col justify-content-between">
         <div>
         <h3 class="title-3">Courses</h3>
@@ -34,17 +18,17 @@
             <table class="table">
               <thead>
                 <tr>
-                  <th scope="col">Name</th>
+                  <th>Title</th>
                   <th scope="col">Since</th>
                   <th scope="col">Final Mark</th>
                 </tr>
               </thead>
               <tbody>
-              @foreach($student->coursesForTeacher() as $course)
+              @foreach($student->coursesForTeacher()->sort() as $course)
                 <tr>
-                  <th scope="row">{{ $course->title}}</th>
-                  <td>{{ $course->pivot->created_at ? date('d-m-Y', $course->pivot->created_at->timestamp) : '-'}}</td>
-                  <td>{{ $course->pivot->final_mark ?? '-'}}</td>
+                  <th class="label" scope="row"><a href="{{ route('course_teacherShow', $course->id) }}">{{ $course->title}}</a></th>
+                  <td>{{ $course->pivot->created_at ? date('d/m/Y', $course->pivot->created_at->timestamp) : '-'}}</td>
+                  <td>{{ $course->pivot->final_mark ? $course->pivot->final_mark.' / 100' : '-'}}</td>
                 </tr>
                 @endforeach
               </tbody>
@@ -55,19 +39,40 @@
         </div>
         <a class="myButton align-self-end mb-2">Manage Enrolments</a>
       </div>
+      
+      <div class="col-12 col-xl-5 form-section layer-2 d-flex flex-col">
+        <h3 class="title-3">Student Info</h3>
+            <div class="label-value">
+              <span>Firstname</span>
+              <span>{{ $student->firstname }}</span>
+            </div>
+            <div class="label-value">
+              <span>Lastname</span>
+              <span>{{ $student->lastname }}</span>
+            </div>
+            <div class="label-value mb-3">
+              <span>Email</span>
+              <span>{{ $student->email }}</span>
+            </div>
+            <hr>
+				    <div class="d-flex flex-col align-items-end mt-3">
+              <a href="{{ route('student_teacherDownloadReport', $student->id) }}"><i class="fas fa-arrow-alt-to-bottom"></i>Download Report</a>
+              <a href="{{ route('student_teacherConfirmDelete', $student->id) }}"><i class="fas fa-trash-alt"></i>Remove Student</a>
+            </div>
+      </div>
     </div>
       
       
-
+@if(count($student->coursesForTeacher())>0)
 
     @foreach($student->coursesForTeacher() as $course)
     
       <h2 class=" block-title light-card layer-2">{{ $course->title}}</h2>
-      <div class="form-section layer-2">
+      <div class="">
         <div class="row">
-        <div class="col-12 col-xl-7">
-          <h4>Chapters</h4>
-          @if(count($course->chapters)===0)
+        <div class="col-12 col-xl-7 form-section layer-2">
+          <h3  class="title-3">Chapters</h3>
+          @if(count($course->chapters->sortBy('order_id'))===0)
             <p>No chapters created</p>
           @else 
           <table class="table">
@@ -75,33 +80,31 @@
                 <tr>
                   <th scope="col">Name</th>
                   <th scope="col">Read</th>
-                  <th scope="col">Assignments Missed</th>
                 </tr>
               </thead>
               <tbody>
             @foreach($course->chapters as $chapter)
                   <tr>
-                    <th scope="row">{{ $chapter->title}}</th>
+                    <th class="label"><a href="{{ route('chapter_teacherShow', $chapter->id)}}">{{ $chapter->title}}</a></th>
                     <td>{{ $chapter->isRead($student->id) ? 'Yes' : '-' }}</td>
-                    <td>TODO</td>
                   </tr>
             @endforeach
               </tbody>
             </table>
             @endif
         </div>
-        <div class="col ml-4">
-          <h4>Skills</h4>
-          @foreach($course->skills as $skill)
+        <div class="col ml-4 form-section layer-2">
+          <h3 class="title-3">Skills</h3>
+          @foreach($course->skills->sort() as $skill)
             <div class="label-value">
               <span>{{ $skill->title}}</span>
-              <span>{{ $skill->studentMark($student->id)!==null ? $skill->studentMark($student->id).' /100' : '-'}}<a href="{{ route('studentSkill_teacherEdit', [$student->id, $skill->id]) }}"><i class="fas fa-pen-square"></i></a></span>
+              <span>{{ $skill->studentMark($student->id)!==null ? $skill->studentMark($student->id).' /100' : '-'}}<a href="{{ route('studentSkill_teacherEdit', [$student->id, $skill->id]) }}"><i class="fas fa-pen-square"></i>Edit</a></span>
             </div>
           @endforeach
         </div>
         </div>
-        <div>
-        <h4>Assignments</h4>
+        <div class="form-section layer-2">
+        <h3 class="title-3">Assignments</h3>
           @if(count($course->assignments)===0)
             <p>No assignments created</p>
           @else 
@@ -109,29 +112,30 @@
               <thead>
                 <tr>
                   <th scope="col">Name</th>
-                  <th scope="col">Submissions</th>
-				          <th scope="col">Questions to Answer</th>
-                  <th scope="col">Mark</th>
+                  <th scope="col" class="cell-center">Submissions</th>
+				          <th scope="col" class="cell-center">Questions to Answer</th>
+                  <th scope="col" class="cell-center">Mark</th>
 				          <th></th>
                 </tr>
               </thead>
               <tbody>
-              @foreach($course->assignments as $assignment)
+              @foreach($course->assignments->sort() as $assignment)
                 <tr>
-                   <th>{{ ucfirst($assignment->title) }}</th>
+                   <td class="label"><a href="{{ route('assignment_teacherShow', $assignment->id)}}">{{ ucfirst($assignment->title) }}</a></td>
               @if($assignment->studentAssignmentByStudent($student->id)==null)
-              <td colspan="3">No submissions</td>
+              <td class="cell-center">-</td>
+              <td colspan="3"></td>
               @else
                
-                   <td>{{ count($assignment->studentAssignmentByStudent($student->id)->submissions) ?? '-'}}</td>
-				          <td>
+                   <td class="cell-center">{{ count($assignment->studentAssignmentByStudent($student->id)->submissions) ?? '-'}}</td>
+				          <td class="cell-center">
                     @if(count($assignment->studentAssignmentByStudent($student->id)->submissions->whereNull('feedback')->whereNotNull('question')))
                       <i class="fas fa-exclamation-square"></i>
                     @else
                       -
                     @endif
                   </td>
-                  <td>
+                  <td class="cell-center">
                   @if($assignment->studentAssignmentByStudent($student->id)->mark) 
                     {{$assignment->studentAssignmentByStudent($student->id)->mark}} / {{ $assignment->max_mark}}
                   @elseif($assignment->studentAssignmentByStudent($student->id)->canBeMarked()) 
@@ -139,7 +143,7 @@
                   @else 
                     -
                   @endif</td>
-					      <td><a href="{{ route('studentAssignment_teacherShow', $assignment->studentAssignmentByStudent($student->id)->id) }}"><i class="fas fa-arrow-alt-square-right greyed"></i>Manage</a></td>
+					        <td><a href="{{ route('studentAssignment_teacherShow', $assignment->studentAssignmentByStudent($student->id)->id) }}"><i class="fas fa-arrow-alt-square-right"></i>Manage</a></td>
                 </tr>
                 @endif
               
@@ -151,6 +155,6 @@
     </div>
 
     @endforeach
-      
+  @endif    
 @endsection
 
